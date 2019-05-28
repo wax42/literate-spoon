11e0b0a9960fe4d086a5de23528b8206d89c2c00
wax42 <vguerand@student.42.fr>
2019-05-28 17:17:22 +0200

    [vguerand] add Main

---
+++ main.py
@@ -0,0 +1,22 @@
+# coding: utf8
+
+
+
+taquin =  [1, 2, 3, 4, 5, 6, 7, 8]
+
+
+def check_valid_taquin(taquin):
+    pass
+
+
+
+def solve_taquin():
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
